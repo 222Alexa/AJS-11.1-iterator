928d4c4249876b0b9b1a8600d7cc5b7390c06ebe
Alexandra Titova <auto-steklo@mail.ru>
2021-03-17 14:35:06 +0300

    fix iterator

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,17 @@
 import characters from './person';
 import Team from './team';
 
+
+
 const team = new Team();
 team.addAll(...characters);
-const char = team.toArray();
 
-for (const key of char) {
+
+for (const key of team) {
   console.log(key);
 }
+
+/*team.members.forEach(item =>{
+console.log(item);
+});
+*/
